9f1d063ba03c49f601014cb5a8cfbe5c71202a96
Javin Carlson <christiancarlsonwork@gmail.com>
2026-01-28 22:44:41 +0100

    Ändrade la till componets hero. Fungerar inte helt fel med refering av first oh last name

---
+++ Project/frontend/src/components/Hero.tsx
@@ -0,0 +1,18 @@
+interface HeroProps {
+    firstName: string,
+    lastName: string,
+    role: string,
+}
+
+export default function Hero({ firstName, lastName, role}: HeroProps) {
+    return (
+        <section className="h-[70vh] flex flex-col items-center justify-center text-center">
+            <h1 className="text-7xl font-black tracking-tighter uppercase">
+                {firstName} <span className="text-blue-500">{lastName}</span>
+            </h1>
+            <p className="text-2xl text-slate-400 mt-4 uppercase tracking-[0.2em]">
+                {role}
+            </p>
+        </section> 
+    )
+}
