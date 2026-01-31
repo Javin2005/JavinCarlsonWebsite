e206033553e5abac8eab70f3f3cb50ca9ded0ffa
Javin Carlson <christiancarlsonwork@gmail.com>
2026-01-31 15:58:08 +0100

    random små ändringar

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -42,7 +42,7 @@ function App(){
       )}
 
       <div className="max-w-6xl mx-auto px-6 py-20">
-        <h2 className="text-3xl font-bold mb-10 text-slate-200">Utvalda Projekt</h2>
+        <h2 className="text-3xl font-bold mb-10 text-slate-200">Sample Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {projects.map((project) => (
             <ProjectCard
